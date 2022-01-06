3aaf49a41f150bffe9a016a8730be825df215fe3
Taylor Anderson <tganderson0@gmail.com>
2022-01-06 10:27:55 -0700

    Everything complete except for the stats screen

--- App.js
+++ App.js
@@ -6,6 +6,8 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Home from "./App/Screens/Home";
 import Stats from "./App/Screens/Stats";
 import Settings from "./App/Screens/Settings";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { faTint, faSignal, faCog } from "@fortawesome/free-solid-svg-icons";
 
 const Tab = createBottomTabNavigator();
 
@@ -13,9 +15,24 @@ const App = () => {
   return (
     <NavigationContainer>
       <Tab.Navigator      >
-        <Tab.Screen name="Home" component={Home} />
-        <Tab.Screen name="Stats" component={Stats} />
-        <Tab.Screen name="Settings" component={Settings} />
+        <Tab.Screen name="Home" component={Home} options={{
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesomeIcon icon={ faTint } color={color} size={size} />
+          )
+        }
+        }/>
+        <Tab.Screen name="Stats" component={Stats} options={{
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesomeIcon icon={ faSignal } color={color} size={size} />
+          )
+        }
+        } />
+        <Tab.Screen name="Settings" component={Settings} options={{
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesomeIcon icon={ faCog } color={color} size={size} />
+          )
+        }
+        }/>
       </Tab.Navigator>
     </NavigationContainer>
   );
